73da1dd3fa1676ade0057ebd4e3a9cf5b3d049fc
faceleft <face-left@mail.ru>
2024-01-28 18:58:59 +0700

    подготовка к слиянию с wifilog

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,10 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+#include "config.h"
 #include "lib/mqtt.h"
-#include "private.h"
-
-void echo_qos0(const char * topic, const char * data, size_t size) {
-    mqtt_publish("echo/0", data, size, 0, 0);
-}
-
-void echo_qos1(const char * topic, const char * data, size_t size) {
-    mqtt_publish("echo/1", data, size, 1, 0);
-}
-
-void echo_qos2(const char * topic, const char * data, size_t size) {
-    mqtt_publish("echo/2", data, size, 2, 0);
-}
-
-fl_topic_t topics_set[] = { //{"name", callback_t, qos}
-    {"test/0", echo_qos0, 0},
-    {"test/1", echo_qos1, 1},
-    {"test/2", echo_qos2, 2},
-};
+#include "lib/log.h"
+#include "lib/tcp.h"
 
 void setup() {
     Serial.begin(115200);
@@ -37,22 +21,6 @@ void setup() {
     }
     puts("\nConnected to the WiFi network");
     printf("Local ESP32 IP: %s\n", WiFi.localIP().toString().c_str());
-
-
-    mqtt_init();
-    mqtt_connect(MQTT_HOST, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD);
-    puts("\nConnecting MQTT");
-    while (mqtt_is_connected() != 1){
-        printf("."); fflush(stdout);
-        delay(100);
-    }
-    puts("\nConnected to the MQTT broker");
-
-    mqtt_subscribe_topics(topics_set, sizeof(topics_set)/sizeof(topics_set[0]));
-    puts("Subscribed");
-
-    puts("end of \"void setup();\"");
-
 }
 
 void loop() {
