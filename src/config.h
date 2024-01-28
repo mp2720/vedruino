b22e31a2a5fb2a4a1780f2b93ab1960340c1b7ce
faceleft <face-left@mail.ru>
2024-01-28 21:29:33 +0700

    сделал активацию в main.cpp

--- src/config.h
+++ src/config.h
@@ -3,10 +3,10 @@
 #define WIFI_SSID "K_LAP2"
 #define WIFI_PASSWD "lapsoft-k_lap"
 
-#define LOG_HOST "192.168.154.64"
+#define LOG_HOST "192.168.111.128"
 #define LOG_PORT "1488"
 
 #define MQTT_HOST LOG_HOST
-#define MQTT_PORT 10104
+#define MQTT_PORT 10103
 #define MQTT_USERNAME "user"
-#define MQTT_PASSWORD ""
+#define MQTT_PASSWORD "R!a120??10EH32"
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <time.h>
 
 #include "config.h"
 #include "lib/mqtt.h"
 #include "lib/log.h"
 #include "lib/tcp.h"
 
-void setup() {
-    Serial.begin(115200);
-    puts("start \"void setup();\"");
+static const char * TAG = "MAIN";
 
-    delay(1000);
-
+void start_wifi() {
     WiFi.mode(WIFI_STA); //Optional
     WiFi.begin(WIFI_SSID, WIFI_PASSWD);
     puts("\nConnecting WIFI");
@@ -21,6 +19,28 @@ void setup() {
     }
     puts("\nConnected to the WiFi network");
     printf("Local ESP32 IP: %s\n", WiFi.localIP().toString().c_str());
+}
+
+void start_log() {
+    log_socket = tcp_connect(LOG_HOST, LOG_PORT);
+    log_output = tcp_log_printf;
+}
+
+void start_mqtt() {
+    mqtt_init();
+    mqtt_connect(MQTT_HOST, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD);
+    while (!mqtt_is_connected()) {
+        printf("."); fflush(stdout);
+        delay(100);
+    }
+    puts("");
+    //mqtt_subscribe_topics(topics, sizeof(topics)/sizeof(topics[0]));
+}
+
+void setup() {
+    start_wifi();
+    start_log();
+    start_mqtt();
 }
 
 void loop() {
