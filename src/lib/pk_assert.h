5549b5a4c72c961fb89f341f4c24fde541e55549
Salomakhin Timur <sergeyrunov22@yandex.ru>
2024-02-26 21:50:23 +0700

    pk_assert

--- src/lib/pk_assert.h
+++ src/lib/pk_assert.h
@@ -2,13 +2,4 @@
 
 #include <assert.h>
 
-#define PK_ASSERT(x) assert(x)
-
-/* #define PK_ASSERT(x) \ */
-/*     do { \ */
-/*         if (!(x)) { \ */
-/*             pk_assert_failed(TAG, #x, __FILE__, __LINE__); \ */
-/*         } \ */
-/*     } while (0) */
-
-/* void pk_assert_failed(const char *expr_str, const char *file, int line); */
+#define PK_ASSERT(x) assert(0)
